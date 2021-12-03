9bdfc380669508054b97170da6d55c99e9072191
arodriguezle <arodriguezle@edu.tecnocampus.cat>
2021-12-03 21:47:18 +0100

    swipe+remove text on small devices

--- js/script_slider.js
+++ js/script_slider.js
@@ -1,13 +1,12 @@
 var slideIndex = 1;
 
-//SEGONS BANNERS AUTO
-const interval = setInterval(function () {
-  plusSlides(1)
+//SEGONS BANNERS AUTO 
+
+let interval = setInterval(function () {
+  plusSlides(1);
 }, 6000);
 
 showSlides(slideIndex);
-
-
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -19,6 +18,9 @@ function currentSlide(n) {
 
 function showSlides(n) {
   clearInterval(interval);
+  interval = setInterval(function () {
+    plusSlides(1);
+  }, 6000);
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
@@ -37,3 +39,15 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+
+var mc = new Hammer.Manager(document.getElementById('slide_container'));
+var swipe = new Hammer.Swipe();
+mc.add(swipe);
+mc.on("swipeleft swiperight", function (ev) {
+  if (ev.type == 'swipeleft') {
+    plusSlides(-1)
+  } else if (ev.type == 'swiperight') {
+    plusSlides(1)
+  }
+});
