68a093537ce8129d2614545a2aacda652d0bb8bf
roserbutron <81020797+roserbutron@users.noreply.github.com>
2024-09-26 23:08:46 +0200

    fix email form

--- src/pages/components/contactForm.tsx
+++ src/pages/components/contactForm.tsx
@@ -1,35 +1,16 @@
 import React, { useState } from "react";
 
 const ContactForm: React.FC = () => {
-	const [email, setEmail] = useState("");
 	const [subject, setSubject] = useState("");
 	const [body, setBody] = useState("");
 
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log(`Email: ${email}\nSubject: ${subject}\nBody: ${body}`);
-		window.open(`mailto:email?subject=${subject}&body=${body}`, "_blank");
+		window.open(`mailto:roserbutron@gmail.com?subject=${subject}&body=${body}`, "_blank");
 	};
 
 	return (
-		<form onSubmit={handleSubmit} className="max-w-md sm:mr-auto mr-2">
-			<div className="mb-4">
-				<label
-					htmlFor="email"
-					className="block text-gray-700 font-bold mb-2"
-				>
-					Email
-				</label>
-				<input
-					type="email"
-					id="email"
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					placeholder="Enter your email"
-					value={email}
-					onChange={(event) => setEmail(event.target.value)}
-					required
-				/>
-			</div>
+		<form onSubmit={handleSubmit} className="w-[350px] max-w-lg mx-auto">
 			<div className="mb-4">
 				<label
 					htmlFor="subject"
@@ -56,7 +37,7 @@ const ContactForm: React.FC = () => {
 				</label>
 				<textarea
 					id="body"
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					className="h-[350px] shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 					placeholder="Enter the message body"
 					value={body}
 					onChange={(event) => setBody(event.target.value)}
@@ -66,7 +47,7 @@ const ContactForm: React.FC = () => {
 			<div className="flex justify-left">
 				<button
 					type="submit"
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+					className="bg-[#ff3f8a] hover:bg-pink-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline  hover:text-[#ffffff] transition duration-200 ease-in-out"
 				>
 					Send
 				</button>
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,9 +2,9 @@ import ContactForm from "./components/contactForm";
 
 const ContactPage = () => {
 	return (
-		<div className="flex h-1/5 w-full mb-8 max-w-[1420px] m-auto min-h-[50vh]">
+		<div className="block md:flex h-1/5 w-full mb-8 max-w-[1420px] m-auto min-h-[50vh]">
 			<img
-				className="ml-8 w-[20vh] h-full object-cover ml-auto mr-2 sm:mr-24"
+				className="md:ml-48 w-[20vh] h-full object-cover mx-auto md:mr-2 sm:mr-24 self-center"
 				src={require("../assets/mail_icon.png")}
 				alt="contact"
 			/>
