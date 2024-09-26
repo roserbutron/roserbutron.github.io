b5bb5cc4222f3a5e8d5df1bbe271d7d9a2a9edaf
roserbutron <81020797+roserbutron@users.noreply.github.com>
2024-09-26 23:15:31 +0200

    remove filter

--- src/pages/components/illustrationGrid.tsx
+++ src/pages/components/illustrationGrid.tsx
@@ -21,30 +21,10 @@ const IllustrationGrid: React.FC<Props> = ({
     768: 1,
   };
 
-  enum filterState {
-    all = "All",
-    illustrations = "Illustrations",
-    collections = "Collections",
-    projects = "Projects",
-  }
-
-  const [selectedFilter, setSelectedFilter] = useState(filterState.all);
-
   const handleChange = () => {};
 
   return (
     <>
-      <div className="w-1/3 z-10 text-center">
-        <Select
-          value={selectedFilter}
-          label={selectedFilter}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </div>
       <Masonry
         breakpointCols={breakpointColumnsObj}
         className="flex gap-4 max-w-[1420px] justify-center m-auto 2sm:mx-8 lg:mx-auto"
